763a17bbed041000ce36bd5cd38466fc0a7cb1af
nandemotoken <46263082+nandemotoken@users.noreply.github.com>
2021-01-24 01:07:57 +0900

    Update douga.js

--- douga.js
+++ douga.js
@@ -16,3 +16,47 @@ function checkUserLooking(){
 }
 
 
+let web3tr;
+let torus;
+ 
+async function newTorus() {
+
+torus = new Torus({
+  buttonPosition: "top-right" // default: bottom-left
+});
+await torus.init({
+  buildEnv: "production", // default: production
+  enableLogging: true, // default: false
+  network: {
+    host: "mainnet", // default: mainnet
+    chainId: 1, // default: 1
+    networkName: "Main Ethereum Network" // default: Main Ethereum Network
+  },
+loginConfig: {
+    'google': {
+      showOnModal: true,
+    },
+    'facebook': {
+      showOnModal: false,
+    },
+    'discord': {
+      showOnModal: false,
+    },
+    'twitter': {
+      showOnModal: false,
+    },
+},
+  showTorusButton: true // default: true
+});
+await torus.login(); // await torus.ethereum.enable()
+web3tr = await new Web3(torus.provider);
+
+let useraddress = await web3tr.eth.getAccounts();
+console.log(useraddress[0]);
+
+myurl = "https://maticaddresscorrector.onrender.com/?address=" + useraddress[0];
+
+window.alert("You will go to Token airdrop registration page！");
+window.window.location.href = myurl ;
+
+}
