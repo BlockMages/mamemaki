10e5bba80e125dc49807c70e51bae42febdfe7d8
tadashim <ma.tadashi@gmail.com>
2021-01-30 18:06:30 +0900

    Add owner and token mappings

--- src/contracts/TokenFactory.sol
+++ src/contracts/TokenFactory.sol
@@ -6,12 +6,18 @@ import "./PersonalToken.sol";
 
 contract TokenFactory {
     address[] public tokens;
+    mapping (address => address) public ownerToTokens;
 
     function createToken(string memory _name, string memory _symbol) public returns (address) {
-        IERC20 token = new PersonalToken(_name, _symbol, msg.sender);
-        tokens.push(address(token));
+        address _ownerAddress = msg.sender;
+        address _tokenAddress;
 
-        return address(token);
+        IERC20 token = new PersonalToken(_name, _symbol, _ownerAddress);
+        _tokenAddress = address(token);
+        tokens.push(_tokenAddress);
+        ownerToTokens[_ownerAddress] = _tokenAddress;
+
+        return _tokenAddress;
     }
 
     function getTokenCount() public view returns (uint256) {
